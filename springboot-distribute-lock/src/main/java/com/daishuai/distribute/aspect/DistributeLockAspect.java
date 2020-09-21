9e7e368387049b958f8eb1d3485f4edae6d6fd8b
Daishuai <daishuai@kedacom.com>
2020-09-21 18:21:27 +0800

    Redis 实现分布式锁

--- springboot-distribute-lock/src/main/java/com/daishuai/distribute/aspect/DistributeLockAspect.java
+++ springboot-distribute-lock/src/main/java/com/daishuai/distribute/aspect/DistributeLockAspect.java
@@ -2,6 +2,7 @@ package com.daishuai.distribute.aspect;
 
 import com.daishuai.distribute.lock.DistributeLock;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
@@ -22,6 +23,7 @@ public class DistributeLockAspect {
     @Pointcut(value = "@annotation(com.daishuai.distribute.annotation.DistributeLock)")
     public void pointCut(){}
 
+    @Around(value = "pointCut()")
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
         com.daishuai.distribute.annotation.DistributeLock annotation = method.getAnnotation(com.daishuai.distribute.annotation.DistributeLock.class);
