245067518463705a1407ad97790f79c4dad9cb20
daishuai <daishuai@kedacom.com>
2023-04-27 11:09:39 +0800

    接收客户端消息

--- springboot-websocket/websocket-server/src/main/java/com/daishuai/websocket/server/controller/DemoController.java
+++ springboot-websocket/websocket-server/src/main/java/com/daishuai/websocket/server/controller/DemoController.java
@@ -1,16 +1,10 @@
 package com.daishuai.websocket.server.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.daishuai.websocket.server.service.KdWebSocketService;
-import com.daishuai.websocket.server.vo.KdWebSocketMsgDefaultVo;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -25,40 +19,15 @@ import java.util.Map;
 @Slf4j
 @RestController
 public class DemoController {
-    
-    @Autowired
-    private KdWebSocketService kdWebSocketService;
 
     @Resource
     private SimpMessagingTemplate simpMessagingTemplate;
 
-    @Resource
-    private StringRedisTemplate stringRedisTemplate;
-
-    @GetMapping(value = "/server/monitor/health/status")
-    public String health() {
-        return "ok";
-    }
     
     
-    @MessageMapping("/personalData/ping")
-    public void personalData(KdWebSocketMsgDefaultVo defaultVo) {
-        String payload = defaultVo.getPayload();
-        String destination = defaultVo.getDestination();
-        String clientId = defaultVo.getClientId();
-        String userId = defaultVo.getUserId();
-        log.info("userId:{}\nclientId:{}\ndestination:{}\npayload:{}\n", userId, destination, clientId, payload);
-        Map<String, Object> message = new HashMap<>();
-        message.put("code", "200");
-        message.put("message", "处理成功");
-        message.put("timestamp", System.currentTimeMillis());
-        message.put("result", defaultVo);
-        defaultVo.setPayload(JSON.toJSONString(message));
-        kdWebSocketService.send(defaultVo);
-        defaultVo.setDestination("/user/" + clientId + "/personalData/pong1");
-        kdWebSocketService.send(defaultVo);
-        defaultVo.setDestination("/user/" + clientId + "/personalData/pong2");
-        kdWebSocketService.send(defaultVo);
+    @MessageMapping("/demo/ping")
+    public void personalData(Map<String, Object> defaultVo) {
+        log.info("接收到客户端发送的消息: {}", JSON.toJSONString(defaultVo));
     }
 
     @Scheduled(cron = "0/3 * * * * ?")
@@ -67,7 +36,6 @@ public class DemoController {
         message.put("code", "200");
         message.put("message", "处理成功");
         message.put("timestamp", System.currentTimeMillis());
-        stringRedisTemplate.opsForValue().set("name", "helloworld");
         simpMessagingTemplate.convertAndSend("/user/demo/pong", message);
     }
     
