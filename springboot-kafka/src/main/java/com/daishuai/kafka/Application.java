d7dd30d051ea85edd17ff49fe557177b7ba1cbe9
daishuai <daishuai@kedacom.com>
2023-05-25 19:57:45 +0800

    kafka stream demo

---
+++ springboot-kafka/src/main/java/com/daishuai/kafka/Application.java
@@ -0,0 +1,18 @@
+package com.daishuai.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Daishuai
+ * @version 1.0.0
+ * @description Kafka示例
+ * @createTime 2023年05月25日 19:34:00
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ springboot-kafka/src/main/java/com/daishuai/kafka/demo/WordCountExample.java
@@ -0,0 +1,48 @@
+package com.daishuai.kafka.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+
+/**
+ * @author Daishuai
+ * @version 1.0.0
+ * @description Kafka流处理-字数统计
+ * @createTime 2023年05月25日 19:36:00
+ */
+public class WordCountExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordCount");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "10.165.77.64:19195,10.165.77.65:19195,10.165.77.66:19195");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> stream = builder.stream("wordCount-input");
+        Pattern pattern = Pattern.compile("\\W+");
+        KStream<String, String> count = stream.flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+                .map((key, value) -> new KeyValue<>(value, value))
+                .filter((key, value) -> !value.equals("the"))
+                .groupByKey()
+                .count()
+                .mapValues(value -> Long.toString(value))
+                .toStream();
+        count.to("wordCount-output");
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), props);
+        kafkaStreams.cleanUp();;
+        kafkaStreams.start();
+        Thread.sleep(5000L);
+        kafkaStreams.close();
+    }
+}
